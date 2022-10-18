4ba8b696923ee2ededad6448b1df48409c9e4009
faturfawkes <nur.faturohman28@gmail.com>
2022-10-18 16:41:15 +0700

    move uploaded file

--- features/user/delivery/handler.go
+++ features/user/delivery/handler.go
@@ -2,34 +2,84 @@ package delivery
 
 import (
 	"bytes"
+	"encoding/base64"
 	"gohub/config"
 	"gohub/features/user/domain"
 	"gohub/middlewares"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
 	"strconv"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type userHandler struct {
 	srv domain.Service
 }
 
+
+
 func New(e *echo.Echo, srv domain.Service) {
 	handler := userHandler{srv: srv}
+	e.Static("/users/update", "./public")
 	e.POST("/login", handler.Login())                                                       // LOGIN USER
 	e.POST("/register", handler.AddUser())                                                  // REGISTER USER
 	e.GET("/users/:id", handler.GetUser(), middleware.JWT([]byte(config.JWT_SECRET)))       // GET USER BY ID
 	e.DELETE("/users", handler.DeleteUser(), middleware.JWT([]byte(config.JWT_SECRET)))     // DELETE USER BY ID
 	e.PUT("/users/update", handler.UpdateUser(), middleware.JWT([]byte(config.JWT_SECRET))) // UPDATE USER BY ID                                  // ADD PROFILE PHOTOS
+}
+
+func upload(c echo.Context) error {
+
+	img, err := c.FormFile("images")
+
+	aws_access_key_id := "AKIATMRW76KP65TYH5H2" 
+	aws_secret_access_key := "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" 
+	token := "" 
+	creds := credentials.NewStaticCredentials(aws_access_key_id, aws_secret_access_key, token)   
+	_, err =  creds.Get() 
+	if err != nil { 
+		return err
+	}   
+	cfg := aws.NewConfig().WithRegion("ap-southeast-1").WithCredentials(creds)   
+	svc := s3.New(session.New(), cfg) 
+	
+	file, err := os.Open(img.Filename)   
+	if err != nil { 
+		return err
+	}   
+	defer file.Close()   
+	fileInfo, _ := file.Stat()   
+	size := fileInfo.Size()   
+	buffer := make([]byte, size) // read file content to buffer 
+	file.Read(buffer)   
+	fileBytes := bytes.NewReader(buffer) 
+	fileType := http.DetectContentType(buffer) 
+	path := "/media/" + file.Name()   
+
+	params := &s3.PutObjectInput{ 
+		Bucket: aws.String("gohubalta"), 
+		Key: aws.String(path), 
+		Body: fileBytes, 
+		ContentLength: aws.Int64(size), 
+		ContentType: aws.String(fileType),
+	}   
+	resp, err := svc.PutObject(params) 
+	if err != nil { 
+		return err
+	}   
+	log.Printf("response %s", awsutil.StringValue(resp)) 
+	return err
 }
 
 func (us *userHandler) Login() echo.HandlerFunc {
@@ -49,80 +99,51 @@ func (us *userHandler) Login() echo.HandlerFunc {
 	}
 }
 
-const (
-	AWS_S3_REGION = "ap-southeast-1"
-	AWS_S3_BUCKET = "gohub-alterra"
-)
-
-const (
-	S3_REGION = "ap-southeast-1"
-	S3_BUCKET = "gohub-alterra"
-)
-
-func uploadFileToS3(fileName string) error {
-
-	s, err := session.NewSession(&aws.Config{
-		Region: aws.String(S3_REGION),
-		Credentials: credentials.NewStaticCredentials(
-			"AKIATMRW76KP65TYH5H2",
-			"zskfWYUM6IwoOU6+2L9Z9Iv3SgUDo7YTKIboFaCK",
-			""),
-	})
-
-	if err != nil {
-		log.Fatal(err, "ERROR SESSION")
-	}
-
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err, "ERROR OPEN FILE")
-		return err
-	}
-	defer file.Close()
-
-	// get the file size and read
-	// the file content into a buffer
-	fileInfo, _ := file.Stat()
-	var size = fileInfo.Size()
-	buffer := make([]byte, size)
-	file.Read(buffer)
-
-	// config settings: this is where you choose the bucket,
-	// filename, content-type and storage class of the file
-	// you're uploading
-	_, s3err := s3.New(s).PutObject(&s3.PutObjectInput{
-		Bucket:               aws.String(S3_BUCKET),
-		Key:                  aws.String(fileName),
-		ACL:                  aws.String("private"),
-		Body:                 bytes.NewReader(buffer),
-		ContentLength:        aws.Int64(size),
-		ContentType:          aws.String(http.DetectContentType(buffer)),
-		ContentDisposition:   aws.String("attachment"),
-		ServerSideEncryption: aws.String("AES256"),
-		StorageClass:         aws.String("INTELLIGENT_TIERING"),
-	})
-
-	return s3err
-}
 
 func (us *userHandler) UpdateUser() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var input UpdateFormat
-		id := middlewares.ExtractToken(c)
 		if err := c.Bind(&input); err != nil {
 			return c.JSON(http.StatusBadRequest, FailResponse("cannot bind input"))
 		}
 
-		image, err := c.FormFile("images")
-		if image != nil {
-			upload := uploadFileToS3(image.Filename)
-			if upload != nil {
-				log.Fatal("ERROR IMAGE")
+		file, err := c.FormFile("images")
+		if file != nil {
+			// Get name
+			name := c.FormValue("name")
+			// Get avatar
+			avatar, err := c.FormFile("images")
+			if err != nil {
+				return err
 			}
+		
+			// Source
+			src, err := avatar.Open()
+			if err != nil {
+				return err
+			}
+			defer src.Close()
+		
+
+			position := strings.Index(avatar.Filename, ",")
+			if position == -1 {
+				// image format doesn't match with 'data:image/png;base64,iVBO...'
+			}
+			// decode the base64 string, removing 'data:image/png;base64,' from it
+			reader := base64.NewDecoder(base64.StdEncoding, bytes.NewBufferString(avatar.Filename[position+1:]))
+
+			data, err := ioutil.ReadAll(reader)
+			if err != nil {
+				// error handler
+			}
+			// you write the file in wherever you want
+			ioutil.WriteFile("public/image.png", data, 0644)
+
+			return c.HTML(http.StatusOK, "<b>Thank you! " + name + "</b>")
 		}
 
-		fixID := uint(id)
-		input.ID = fixID
+		id := middlewares.ExtractToken(c)
+		input.ID = uint(id)
 		cnv := ToDomain(input)
 		_, err = us.srv.UpdateUser(cnv)
 		if err != nil {
