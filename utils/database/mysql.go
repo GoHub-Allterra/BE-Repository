d13d8dc3403712a08f2361ce08b5d05f3b6fe9a4
faturfawkes <nur.faturohman28@gmail.com>
2022-10-19 11:07:26 +0700

    test ci/cd

--- utils/database/mysql.go
+++ utils/database/mysql.go
@@ -5,23 +5,21 @@ import (
 	posts "gohub/features/post/repository"
 	user "gohub/features/user/repository"
 
-	"fmt"
-
 	"github.com/labstack/gommon/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB(c *config.AppConfig) *gorm.DB {
-	str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		c.DBUser,
-		c.DBPwd,
-		c.DBHost,
-		c.DBPort,
-		c.DBName,
-	)
+	// str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	// 	c.DBUser,
+	// 	c.DBPwd,
+	// 	c.DBHost,
+	// 	c.DBPort,
+	// 	c.DBName,
+	// )
 
-	// str := "root:@tcp(mysql:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
+	str := "root:@tcp(mysql:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
 	if err != nil {
