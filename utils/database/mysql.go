d53c9deda798919dded2ccb1bc69314711475f47
faturfawkes <nur.faturohman28@gmail.com>
2022-10-19 11:40:07 +0700

    fixing docker

--- utils/database/mysql.go
+++ utils/database/mysql.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"fmt"
+	// "fmt"
 	"gohub/config"
 	posts "gohub/features/post/repository"
 	user "gohub/features/user/repository"
@@ -12,15 +12,15 @@ import (
 )
 
 func InitDB(c *config.AppConfig) *gorm.DB {
-	str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		c.DBUser,
-		c.DBPwd,
-		c.DBHost,
-		c.DBPort,
-		c.DBName,
-	)
+	// str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	// 	c.DBUser,
+	// 	c.DBPwd,
+	// 	c.DBHost,
+	// 	c.DBPort,
+	// 	c.DBName,
+	// )
 
-	// str := "root:@tcp(localhost:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
+	str := "root:@tcp(localhost:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
 	if err != nil {
