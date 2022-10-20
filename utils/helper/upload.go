26d58367b43424bca7714ed0807df8e32d64dab6
root <heribudiyana@gmail.com>
2022-10-20 14:43:12 +0700

    fixing posts

--- utils/helper/upload.go
+++ utils/helper/upload.go
@@ -17,7 +17,7 @@ import (
 const charset = "abcdefghijklmnopqrstuvwxyz" +
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-	var seededRand *rand.Rand = rand.New(
+var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
 func autoGenerate(length int, charset string) string {
@@ -31,7 +31,6 @@ func autoGenerate(length int, charset string) string {
 func String(length int) string {
 	return autoGenerate(length, charset)
 }
-
 
 func UploadProfile(c echo.Context) (string, error) {
 
@@ -52,10 +51,38 @@ func UploadProfile(c echo.Context) (string, error) {
 	uploader := s3manager.NewUploader(s3Session)
 
 	input := &s3manager.UploadInput{
-		Bucket:      aws.String("gohubalta"),   // bucket's name
-		Key:         aws.String("profile/"+randomStr+"-"+fileheader.Filename), // files destination location
-		Body:        file,      // content of the file
-		ContentType: aws.String("image/jpg"),   // content type
+		Bucket:      aws.String("gohubalta"),                                        // bucket's name
+		Key:         aws.String("profile/" + randomStr + "-" + fileheader.Filename), // files destination location
+		Body:        file,                                                           // content of the file
+		ContentType: aws.String("image/jpg"),                                        // content type
+	}
+	res, err := uploader.UploadWithContext(context.Background(), input)
+	return res.Location, err
+}
+
+func UploadPosts(c echo.Context) (string, error) {
+
+	file, fileheader, err := c.Request().FormFile("images")
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+
+	randomStr := String(20)
+
+	s3Config := &aws.Config{
+		Region:      aws.String("ap-southeast-1"),
+		Credentials: credentials.NewStaticCredentials("ACCESS_KEY_IAM", "SECRET_KEY_IAM", ""),
+	}
+	s3Session := session.New(s3Config)
+
+	uploader := s3manager.NewUploader(s3Session)
+
+	input := &s3manager.UploadInput{
+		Bucket:      aws.String("gohubalta"),                                      // bucket's name
+		Key:         aws.String("posts/" + randomStr + "-" + fileheader.Filename), // files destination location
+		Body:        file,                                                         // content of the file
+		ContentType: aws.String("image/jpg"),                                      // content type
 	}
 	res, err := uploader.UploadWithContext(context.Background(), input)
 	return res.Location, err
