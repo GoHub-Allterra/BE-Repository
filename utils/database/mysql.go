0e15fcacb29e4ce46033a9470f8fbac7a88064c2
faturfawkes <nur.faturohman28@gmail.com>
2022-10-21 01:19:16 +0700

    run docker

--- utils/database/mysql.go
+++ utils/database/mysql.go
@@ -1,51 +1,12 @@
-// package database
-
-// import (
-// 	// "gohub/config"
-// 	"os"
-// 	// "fmt"
-// 	comments "gohub/features/comments/repository"
-// 	posts "gohub/features/post/repository"
-// 	user "gohub/features/user/repository"
-
-// 	"github.com/labstack/gommon/log"
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// )
-
-// func InitDB() *gorm.DB {
-
-// 	str := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PWD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-
-// 	// str := fmt.Sprint("root:@tcp(127.0.0.1:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local")
-// 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
-// 	if err != nil {
-// 		log.Error("db config error :", err)
-// 		return nil
-// 	}
-// 	migrateDB(db)
-// 	return db
-// }
-
-// func migrateDB(db *gorm.DB) {
-// 	db.AutoMigrate(&comments.Comments{})
-// 	db.AutoMigrate(&user.User{})
-// 	db.AutoMigrate(&posts.Post{})
-// }
-
 package database
 
 import (
 	// "gohub/config"
-	// "os"
-	"fmt"
-	// "gohub/config"
-
+	"os"
+	// "fmt"
 	comments "gohub/features/comments/repository"
 	posts "gohub/features/post/repository"
 	user "gohub/features/user/repository"
-
-	// "fmt"
 
 	"github.com/labstack/gommon/log"
 	"gorm.io/driver/mysql"
@@ -53,22 +14,13 @@ import (
 )
 
 func InitDB() *gorm.DB {
-// 	// str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-// 	// 	c.DBUser,
-// 	// 	c.DBPwd,
-// 	// 	c.DBHost,
-// 	// 	c.DBPort,
-// 	// 	c.DBName,
-// 	// )
 
-	// str := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PWD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	str := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PWD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-	str := fmt.Sprint("root:@tcp(127.0.0.1:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local")
-	// str := "root:@tcp(mysql:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
-
+	// str := fmt.Sprint("root:@tcp(127.0.0.1:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
 	if err != nil {
-		log.Error("db config error :", err.Error())
+		log.Error("db config error :", err)
 		return nil
 	}
 	migrateDB(db)
@@ -76,7 +28,55 @@ func InitDB() *gorm.DB {
 }
 
 func migrateDB(db *gorm.DB) {
+	db.AutoMigrate(&comments.Comments{})
 	db.AutoMigrate(&user.User{})
 	db.AutoMigrate(&posts.Post{})
-	db.AutoMigrate(&comments.Comments{})
 }
+
+// package database
+
+// import (
+// 	// "gohub/config"
+// 	"os"
+// 	// "fmt"
+// 	// "gohub/config"
+
+// 	comments "gohub/features/comments/repository"
+// 	posts "gohub/features/post/repository"
+// 	user "gohub/features/user/repository"
+
+// 	// "fmt"
+
+// 	"github.com/labstack/gommon/log"
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// func InitDB() *gorm.DB {
+// // 	// str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+// // 	// 	c.DBUser,
+// // 	// 	c.DBPwd,
+// // 	// 	c.DBHost,
+// // 	// 	c.DBPort,
+// // 	// 	c.DBName,
+// // 	// )
+
+// 	str := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PWD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+// 	// str := fmt.Sprint("root:@tcp(127.0.0.1:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local")
+// 	// str := "root:@tcp(mysql:3306)/gohub?charset=utf8mb4&parseTime=True&loc=Local"
+
+// 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
+// 	if err != nil {
+// 		log.Error("db config error :", err.Error())
+// 		return nil
+// 	}
+// 	migrateDB(db)
+// 	return db
+// }
+
+// func migrateDB(db *gorm.DB) {
+// 	db.AutoMigrate(&user.User{})
+// 	db.AutoMigrate(&posts.Post{})
+// 	db.AutoMigrate(&comments.Comments{})
+// }
